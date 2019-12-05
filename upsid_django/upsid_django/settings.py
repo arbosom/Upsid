fb077ac24fdc486117a319c001a7dd4b7a945156
Estelle LABIDURIE <estelle.labidurie@sfr.fr>
2019-12-05 13:32:29 +0100

    settings django db

--- upsid_django/upsid_django/settings.py
+++ upsid_django/upsid_django/settings.py
@@ -75,8 +75,18 @@ WSGI_APPLICATION = 'upsid_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'UPSID',
+        'USER' : 'estelle',
+        'PASSWORD' : 'connectSQL',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+
+
+
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
